06f286d719cdc7ad64d07cc1bfd63b9559c1f5a3
SvZ-web <svetlana.zygiene@gmail.com>
2024-08-11 19:24:09 +0300

    new-js

--- scripts.js
+++ scripts.js
@@ -13,9 +13,16 @@ function showSlides() {
   setTimeout(showSlides, 2000);}
 
 
+document.getElementById("antraste").addEventListener("mouseenter", mouseEnter);
+document.getElementById("antraste").addEventListener("mouseleave", mouseLeave);
+
+function mouseEnter() {/*from  w ww.j  a  v a2s  . c o  m*/
+    document.getElementById("antraste").style.color = "red";
+}
+
+function mouseLeave() {
+    document.getElementById("antraste").style.color = "black";
+}
+
 function new_text()
 {document.getElementById ('naudinga').innerHTML="Dėkojame už jūsų nuomonę."}
-  
-
-
-
